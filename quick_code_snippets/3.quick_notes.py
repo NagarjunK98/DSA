bbd775fb9877db71e81c5c99c199c6c34bd74606
NagarjunK98 <53399371+nagarjunkumar@users.noreply.github.com>
2024-05-29 20:37:25 +0530

    Added quick notes file

---
+++ quick_code_snippets/3.quick_notes.py
@@ -0,0 +1,15 @@
+# To assign maximum integer value
+import sys
+val = sys.maxsize
+
+# to loop through integer > 10 or string of characters
+value = 10  # or value = "ABC" 
+for i in str(value):
+    print(i)
+
+
+# to combine 2 lists
+l1 = [1,2,3,4]
+l2 = [5,6,7,8]
+res = [*l1, *l2]
+print(res) # output - [1,2,3,4,5,6,7,8]
